9fcd2eee3b4f25a279438b73fe7bb3607c8529b3
HoangVX <hoangvx@rikkeisoft.com>
2021-11-11 16:33:15 +0700

    1

--- _helpers/db.js
+++ _helpers/db.js
@@ -21,11 +21,10 @@ async function initialize() {
   const sequelize = new Sequelize(database, user, password, host, port, {
     dialect: "mysql",
     pool: {
-      max: 15,
-      min: 5,
-      idle: 50000,
-      evict: 50000,
-      acquire: 50000,
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
     },
   });
 
