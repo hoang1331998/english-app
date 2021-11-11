bfa8d65fbef4e7c4d44c5052fb048a8c8d467d4b
HoangVX <hoangvx@rikkeisoft.com>
2021-11-11 15:37:30 +0700

    2

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use("/games", require("./games/games.controller"));
 app.use(errorHandler);
 
 // start server
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server listening on port " + port));
