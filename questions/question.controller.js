009acf9a0f4bc2917eb86f1ed54661add0dd539d
HoangVX <hoangvx@rikkeisoft.com>
2021-11-23 09:40:37 +0700

    unrequired question

--- questions/question.controller.js
+++ questions/question.controller.js
@@ -1,98 +1,113 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Joi = require('joi');
-const validateRequest = require('../_middleware/validate-request');
-const authorize = require('../_middleware/authorize')
-const questionService = require('./question.service');
+const Joi = require("joi");
+const validateRequest = require("../_middleware/validate-request");
+const authorize = require("../_middleware/authorize");
+const questionService = require("./question.service");
 
 // routes
-router.post('/', authorize(), createQuestionSchema, createQuestion);
-router.get('/', authorize(), getAll);
-router.get('/:id', authorize(), getById);
-router.put('/:id', authorize(), updateSchema, update);
-router.delete('/:id', authorize(), _delete);
+router.post("/", authorize(), createQuestionSchema, createQuestion);
+router.get("/", authorize(), getAll);
+router.get("/:id", authorize(), getById);
+router.put("/:id", authorize(), updateSchema, update);
+router.delete("/:id", authorize(), _delete);
 
 module.exports = router;
 
 function createQuestionSchema(req, res, next) {
-    const schema = Joi.object({
-        questionName: Joi.string().required(),
-        questionType: Joi.number().required(),
-        questionTitle: Joi.string().required(),
-        questionContent: Joi.string(),
-        questionDescription: Joi.string(),
-        questionLevel: Joi.number().required(),
-        questionCaregory: Joi.number(),
-        questionAnswer: Joi.string().required(),
-        questionPoint: Joi.number().required(),
-        questionExam: Joi.number(),
-        createdBy: Joi.number().required(),
-    });
-    validateRequest(req, next, schema);
+  const schema = Joi.object({
+    questionName: Joi.string().required(),
+    questionType: Joi.number().required(),
+    questionTitle: Joi.string().required(),
+    questionContent: Joi.string() || null,
+    questionDescription: Joi.string(),
+    questionLevel: Joi.number().required(),
+    questionCaregory: Joi.number(),
+    questionAnswer: Joi.string().required(),
+    questionPoint: Joi.number().required(),
+    questionExam: Joi.number(),
+    createdBy: Joi.number().required(),
+  });
+  validateRequest(req, next, schema);
 }
 
 function createQuestion(req, res, next) {
-    questionService.create(req.body)
-        .then(() => res.json({
-            code: 200,
-            message: 'Created successfully',
-            data: []
-        }))
-        .catch(next);
+  questionService
+    .create(req.body)
+    .then(() =>
+      res.json({
+        code: 200,
+        message: "Created successfully",
+        data: [],
+      })
+    )
+    .catch(next);
 }
 
 function getAll(req, res, next) {
-    questionService.getAll()
-        .then(questions => res.json({
-            code: 200,
-            message: "Successfully",
-            data: questions
-        }))
-        .catch(next);
+  questionService
+    .getAll()
+    .then((questions) =>
+      res.json({
+        code: 200,
+        message: "Successfully",
+        data: questions,
+      })
+    )
+    .catch(next);
 }
 
 function getById(req, res, next) {
-    questionService.getById(req.params.id)
-        .then(question => res.json({
-            code: 200,
-            message: "Successfully",
-            data: question
-        }))
-        .catch(next);
+  questionService
+    .getById(req.params.id)
+    .then((question) =>
+      res.json({
+        code: 200,
+        message: "Successfully",
+        data: question,
+      })
+    )
+    .catch(next);
 }
 
 function updateSchema(req, res, next) {
-    const schema = Joi.object({
-        questionName: Joi.string().empty(''),
-        questionType: Joi.number().empty(''),
-        questionTitle: Joi.string().empty(''),
-        questionContent: Joi.string(),
-        questionDescription: Joi.string().empty(''),
-        questionLevel: Joi.number().empty(''),
-        questionCaregory: Joi.number().empty(''),
-        questionAnswer: Joi.string().empty(''),
-        questionPoint: Joi.number().empty(''),
-        questionExam: Joi.number().empty(''),
-        createdBy: Joi.number().empty(''),
-    });
-    validateRequest(req, next, schema);
+  const schema = Joi.object({
+    questionName: Joi.string().empty(""),
+    questionType: Joi.number().empty(""),
+    questionTitle: Joi.string().empty(""),
+    questionContent: Joi.string(),
+    questionDescription: Joi.string().empty(""),
+    questionLevel: Joi.number().empty(""),
+    questionCaregory: Joi.number().empty(""),
+    questionAnswer: Joi.string().empty(""),
+    questionPoint: Joi.number().empty(""),
+    questionExam: Joi.number().empty(""),
+    createdBy: Joi.number().empty(""),
+  });
+  validateRequest(req, next, schema);
 }
 
 function update(req, res, next) {
-    questionService.update(req.params.id, req.body)
-        .then(question => res.json({
-            code: 200,
-            message: "Update successfully",
-            data: question
-        }))
-        .catch(next);
+  questionService
+    .update(req.params.id, req.body)
+    .then((question) =>
+      res.json({
+        code: 200,
+        message: "Update successfully",
+        data: question,
+      })
+    )
+    .catch(next);
 }
 
 function _delete(req, res, next) {
-    questionService.delete(req.params.id)
-        .then(() => res.json({
-            code: 200,
-            message: 'Deleted successfully'
-        }))
-        .catch(next);
+  questionService
+    .delete(req.params.id)
+    .then(() =>
+      res.json({
+        code: 200,
+        message: "Deleted successfully",
+      })
+    )
+    .catch(next);
 }
